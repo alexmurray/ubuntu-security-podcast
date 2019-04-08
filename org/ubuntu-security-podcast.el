045abf6416f0430b7f655d2675ec1d2d3f16eebe
Alex Murray <murray.alex@gmail.com>
2019-04-08 22:25:41 +0930

    Update elisp to handle recent workaround in castanet

--- org/ubuntu-security-podcast.el
+++ org/ubuntu-security-podcast.el
@@ -172,13 +172,12 @@
                                             "~/Ubuntu Security Podcast/"))
          (num-bytes (file-attribute-size (file-attributes audio-file-name)))
          (output (shell-command-to-string (format "ffmpeg -i \"%s\"" audio-file-name))))
-    (message "%s" output)
     (if (null (string-match "Duration: [0-9]\\{2\\}:\\([0-9]\\{2\\}\\):\\([0-9]\\{2\\}\\).[0-9]\\{2\\}"
                             output))
         (error "Unable to get Duration of %s via ffmpeg" audio-file-name)
       (let ((mins (match-string 1 output))
             (secs (match-string 2 output)))
-        (re-search-forward "^:EXPORT_HUGO_CUSTOM_FRONT_MATTER: :episode_image img/episode/default.png :explicit no :podcast_file USP_E[0-9]+\.mp3 :podcast_duration \"\\([0-9M]\\{2\\}:[0-9S]\\{2\\}\\)\" :podcast_bytes \"\\(NUM_BYTES\\|[0-9]\+\\)\"$")
+        (re-search-forward "^:EXPORT_HUGO_CUSTOM_FRONT_MATTER: :episode_image img/episode/default.png :explicit no :podcast_file USP_E[0-9]+\.mp3 :podcast_duration \"\\([0-9M]\\{2\\}:[0-9S]\\{2\\}\\)\" :podcast_bytes \"\\(NUM_BYTES\\|[0-9]\+\\)\" :permalink \"https://ubuntusecuritypodcast.org/episode-[0-9]+/\"$")
         (replace-match (concat mins ":" secs) t t nil 1)
         (replace-match (format "%d" num-bytes) t t nil 2)))))
 
