7dea62d816705c06e373fcb8d25874b6e789cba5
Alex Murray <murray.alex@gmail.com>
2022-02-25 14:41:49 +1030

    Set duration as seconds only as this is preferred by itunes etc
    Signed-off-by: Alex Murray <murray.alex@gmail.com>

--- org/ubuntu-security-podcast.el
+++ org/ubuntu-security-podcast.el
@@ -177,20 +177,17 @@
            (labels-file-name (expand-file-name (format "USP_E%03d_labels.txt" episode)
                                                "~/Ubuntu Security Podcast/"))
            (num-bytes (file-attribute-size (file-attributes audio-file-name)))
-           (output (shell-command-to-string (format "ffprobe -i \"%s\" -show_entries format=duration -sexagesimal -v quiet -of csv=\"p=0\"" audio-file-name))))
+           (output (string-trim (shell-command-to-string (format "ffprobe -i \"%s\" -show_entries format=duration -v quiet -of csv=\"p=0\"" audio-file-name))))
+           (duration (string-to-number output)))
+      (when (= duration 0)
+        (error "Unable to get duration of %s from ffprobe output: '%s'" audio-file-name output))
       (save-excursion
         ;; first update export date
         (re-search-forward "^:EXPORT_DATE: \\(.*\\)$")
         (replace-match (format-time-string "%Y-%m-%d %H:%M") t t nil 1)
-        (if (null (string-match "^\\([0-9]*\\):\\([0-9]\\{2\\}\\):\\([0-9]\\{2\\}\\).[0-9]\\{2\\}.*$"
-                                output))
-            (error "Unable to get Duration of %s via ffmpeg" audio-file-name)
-          (let ((hrs (string-to-number (match-string 1 output)))
-                (mins (string-to-number (match-string 2 output)))
-                (secs (string-to-number (match-string 3 output))))
-            (re-search-forward "^:EXPORT_HUGO_CUSTOM_FRONT_MATTER: :episode_image img/usp_logo_500.png :explicit no :podcast_file USP_E[0-9]+\.mp3 :podcast_duration \"\\(\\([0-9H]*:\\)?[0-9M]\\{2\\}:[0-9S]\\{2\\}\\)\" :podcast_bytes \"\\(NUM_BYTES\\|[0-9]\+\\)\" :permalink \"https://ubuntusecuritypodcast.org/episode-[0-9]+/\" :guid [a-z0-9]+$")
-            (replace-match (format "%d:%02d:%02d" hrs mins secs) t t nil 1)
-            (replace-match (format "%d" num-bytes) t t nil 3)))
+        (re-search-forward "^:EXPORT_HUGO_CUSTOM_FRONT_MATTER: :episode_image img/usp_logo_500.png :explicit no :podcast_file USP_E[0-9]+\.mp3 :podcast_duration \"\\(.*\\)\" :podcast_bytes \"\\(NUM_BYTES\\|[0-9]\+\\)\" :permalink \"https://ubuntusecuritypodcast.org/episode-[0-9]+/\" :guid [a-z0-9]+$")
+        (replace-match (format "%d" duration) t t nil 1)
+        (replace-match (format "%d" num-bytes) t t nil 2)
       (save-excursion
         ;; now update timestamps for each heading when available
         (let ((labels))
